4c4e947cf4ab82e44ca4ea84421a5381c34e0cca
neeluk7 <neelukalani7@gmail.com>
2026-02-04 13:31:31 +0100

    Updated script to mount tyche driver on FPGA platform with initramfs

--- C/scripts/riscv64_install_drivers.sh
+++ C/scripts/riscv64_install_drivers.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-#sudo insmod /tyche/drivers/tyche.ko
-sudo chmod 777 /dev/tyche
-sudo cp -r /tyche/programs /root
-sudo cp -r /tyche/benchmarks /root
+# While using disk or running on VF2 
+
+# #sudo insmod /tyche/drivers/tyche.ko
+
+#sudo chmod 777 /dev/tyche
+#sudo cp -r /tyche/programs /root
+#sudo cp -r /tyche/benchmarks /root
 
 # The commands to use in the ramfs are below, use them when running with the ramfs instead of the disk: 
+
 #insmod /tyche/drivers/tyche.ko
 #mknod /dev/tyche c 244 0
 #chmod 777 /dev/tyche
+
+# For XiangShan on FPGA with initramfs 
+mount -t devtmpfs devtmpfs /dev
